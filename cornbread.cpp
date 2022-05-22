2001f9cd9d52660d57779146d45fd960cde8be19
dwayne brock <milburndbrock@gmail.com>
2022-05-21 22:20:11 -0400

    Add files via upload

---
+++ cornbread.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+// MAKES ONE STANDARD PAN OF CORNBREAD
+//ALL COUT STATEMENTS
+
+int main()
+{
+    std::cout << "\nBEGINNING OF PROGRAM.\n" << std::endl;
+    
+    std::cout << "\nCornbread Recipe\n" << std:: endl;
+    std::cout << "\tIngredients:\n" << std::endl;
+    std::cout << "\t\t1/4 cup of butter" << std::endl;
+    std::cout << "\t\t1 cup milk" << std::endl;
+    std::cout << "\t\t1 large egg" << std::endl;
+    std::cout << "\t\t1 1/4 cups cornbread mix" << std::endl;
+    std::cout << "\t\t1 cup all purpose flour" << std::endl;
+    std::cout << "\t\t1/2 cup granulated sugar" << std::endl;
+    std::cout << "\t\t1 tablespoon baking powder" << std::endl;
+    std::cout << "\t\t1/2 teaspoon salt\n" << std::endl;
+
+    std::cout << "\tDirections:\n" << std::endl;
+    std::cout << "\t\t1:\tMix all the ingredients thoroughly in a mixing bowl." << std::endl;
+    std::cout << "\t\t2:\tPreheat oven to 400 degrees Farenheit." << std::endl;
+    std::cout << "\t\t3:\tPour the mix into a greased 8\" baking pan." << std::endl;
+    std::cout << "\t\t4:\tBake for 20 -25 minutes. Test with toothpick." << std::endl;
+    std::cout << "\t\t5:\tremove cornbread from pan and let cool to taste." << std::endl;
+    
+    std::cout << "\nEND OF PROGRAM.\n" << std::endl;
+    
+    return 0;
+}
